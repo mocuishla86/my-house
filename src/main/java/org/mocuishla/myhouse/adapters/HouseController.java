35e8671017e27a5ccee4b554ab4f8a1e3a15cfe8
isabel.llanes <isabel.llanes@thougthworks.com>
2022-07-22 10:40:13 +0200

    Create Rest input adapter

---
+++ src/main/java/org/mocuishla/myhouse/adapters/HouseController.java
@@ -0,0 +1,29 @@
+package org.mocuishla.myhouse.adapters;
+
+import org.mocuishla.myhouse.adapters.fake.FakeActionRepository;
+import org.mocuishla.myhouse.adapters.fake.FakeAirConditioner;
+import org.mocuishla.myhouse.domain.House;
+import org.mocuishla.myhouse.domain.ports.Action;
+import org.mocuishla.myhouse.domain.ports.AirConditioner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HouseController {
+
+    private House house = new House(new FakeAirConditioner(), new FakeActionRepository());
+
+    @PostMapping("/temperature")
+    public void registerTemperature(@RequestBody TemperatureDTO temperatureDTO){
+        house.setTemperature(temperatureDTO.getTemperature());
+    }
+
+    @GetMapping("/actions")
+    public List<Action> getActions(){
+        return house.getAllActions();
+    }
+}
---
+++ src/main/java/org/mocuishla/myhouse/adapters/TemperatureDTO.java
@@ -0,0 +1,15 @@
+package org.mocuishla.myhouse.adapters;
+
+public class TemperatureDTO {
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    private double temperature;
+
+}
