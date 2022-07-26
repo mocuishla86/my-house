b41a629e13f6e8631dbd5bfba334e3e1bce54ab4
isabel.llanes <isabel.llanes@thougthworks.com>
2022-07-26 17:20:43 +0200

    create HumidityDTO

---
+++ src/main/java/org/mocuishla/myhouse/adapters/HumidityDTO.java
@@ -0,0 +1,13 @@
+package org.mocuishla.myhouse.adapters;
+
+public class HumidityDTO {
+    private int humidity;
+
+    public int getHumidity(){
+        return humidity;
+    }
+
+    public void setHumidity(int humidity){
+        this.humidity = humidity;
+    }
+}
