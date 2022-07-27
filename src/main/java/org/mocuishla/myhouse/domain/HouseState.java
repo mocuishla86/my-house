8cb1e1f8ff950508f50d68282f89089da930045b
isabel.llanes <isabel.llanes@thougthworks.com>
2022-07-27 11:41:06 +0200

    create a new class houseState

---
+++ src/main/java/org/mocuishla/myhouse/domain/HouseState.java
@@ -0,0 +1,27 @@
+package org.mocuishla.myhouse.domain;
+
+public class HouseState {
+
+    private double temperature;
+    private int humidity;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+
+
+
+}
