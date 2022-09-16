66fb4eeeb88120941edefae914e687d624427239
isabel.llanes <isabel.llanes@thougthworks.com>
2022-09-16 12:25:09 +0200

    test postgres Repository

---
+++ src/test/java/org/mocuishla/myhouse/adapters/output/persistence/postgres/PostgresActionRepositoryTest.java
@@ -0,0 +1,36 @@
+package org.mocuishla.myhouse.adapters.output.persistence.postgres;
+
+import org.junit.jupiter.api.Test;
+import org.mocuishla.myhouse.domain.business.model.Action;
+import org.mocuishla.myhouse.domain.business.model.ActionType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PostgresActionRepositoryTest {
+
+    @Autowired
+    private JpaActionRepository jpaActionRepository;
+
+    @Test
+    void shouldSaveAction() {
+        PostgresActionRepository sut = new PostgresActionRepository(jpaActionRepository);
+        Action action = new Action(LocalDateTime.now(), ActionType.TurnFreshAirOn, 44, 34);
+
+        sut.saveAction(action);
+        List<Action> actions = sut.getAllActions();
+
+        assertThat(actions).anyMatch(actionInList ->
+                actionInList.getId().equals(action.getId())
+                        && actionInList.getTimestamp().equals(action.getTimestamp())
+                        && actionInList.getTemperature() == 44
+                        && actionInList.getHumidity() == 34
+                        && actionInList.getType().equals(ActionType.TurnFreshAirOn));
+    }
+
+}
